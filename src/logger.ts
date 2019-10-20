8585df9789645bff8b1487067534cbfa2c05ffcf
Hugo Grochau <hugo@grochau.com>
2019-10-20 00:35:28 -0300

    Set all loggers to console.info

--- src/logger.ts
+++ src/logger.ts
@@ -2,8 +2,8 @@ import chalk from 'chalk'
 import { options } from './options'
 
 export const logger = {
-  debug: (message?: string | null) => options.verbose && console.debug(`${chalk.bold.blue('DEBUG')} ${message}`),
+  debug: (message?: string | null) => options.verbose && console.info(`${chalk.bold.blue('DEBUG')} ${message}`),
   info: (message?: string | null) => console.info(`${chalk.bold.green('INFO')} ${message}`),
-  warn: (message?: string | null) => console.warn(`${chalk.bold.yellow('WARN')} ${message}`),
-  error: (message?: string | null) => console.error(`${chalk.bold.red('ERROR')} ${message}`)
+  warn: (message?: string | null) => console.info(`${chalk.bold.yellow('WARN')} ${message}`),
+  error: (message?: string | null) => console.info(`${chalk.bold.red('ERROR')} ${message}`)
 }
